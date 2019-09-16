3840386587e8397ff16e246d02d23d8a580ed7e6
Andrew Lott <AndrewLott@gmail.com>
2019-09-16 02:02:40 -0700

    Add d6 animation

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class Dice : MonoBehaviour {
-    public RawImage face;
+    public List<RawImage> faces;
     public List<Texture> sides;
 
     public void SetFace(int index) {
-        face.texture = sides[index];
+        List<int> usedIndexes = new List<int> { index };
+        faces[0].texture = sides[index];
+        for (int i = 1; i < faces.Count; i++) {
+            int idx = Utils.RandomInt(sides.Count);
+            while (usedIndexes.Contains(idx)) {
+                idx = Utils.RandomInt(sides.Count);
+            }
+            faces[i].texture = sides[idx];
+            usedIndexes.Add(idx);
+        }
     }
 }
